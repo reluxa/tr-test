5213c0b063795d91225ccbba0c534b2a41d3d451
reluxa <reluxa@gmail.com>
2015-06-15 01:24:02 +0200

    trrr

--- src/main/java/org/reluxa/Launcher.java
+++ src/main/java/org/reluxa/Launcher.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Launcher {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
     InputSender inputSender = ctx.getBean(InputSender.class);
     DBStore dbStore = ctx.getBean(DBStore.class);
     dbStore.deleteAll();
+    Thread.sleep(5000);
     for (int i=0;i<10;i++) {
       inputSender.send(i);
+      //Thread.sleep(1000);
     }
+
+    Thread.sleep(2000);
+    ctx.close();
   }
 
 }
--- src/main/java/org/reluxa/service/DBStore.java
+++ src/main/java/org/reluxa/service/DBStore.java
@@ -1,5 +1,7 @@
 package org.reluxa.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,21 +11,27 @@ import javax.sql.DataSource;
 public class DBStore {
 
   public final JdbcTemplate jdbcTemplatel;
+  private Logger logger = LoggerFactory.getLogger(DBStore.class);
+
 
   public DBStore(DataSource dataSource) {
     this.jdbcTemplatel = new JdbcTemplate(dataSource);
   }
 
   public void deleteAll() {
-    jdbcTemplatel.execute("delete from squares");
+    jdbcTemplatel.execute("delete from squares2");
+    logger.info("All entries were deleted from the database");
   }
 
   public void insert(int number) {
-    jdbcTemplatel.execute("insert into squares(id) values("+number+")");
+    jdbcTemplatel.execute("insert into squares2(id) values("+number+")");
+    logger.info("{} was inserted to the database", number);
+
   }
 
   public void addResult(int number, int result) {
-    jdbcTemplatel.execute("update squares set square = "+result+" where id = "+number);
+    jdbcTemplatel.execute("update squares2 set square = "+result+" where id = "+number);
+    logger.info("{} was updated in the db to {}", number, result);
   }
 
 
